d825f2e0711393bca4f1ca9db31f5630ebfbbddd
Pooja Shivaraya (MS/EHT-ES) <shivaraya.pooja@in.bosch.com>
2025-01-08 10:20:37 +0530

    Added multiply class

---
+++ Multiply.java
@@ -0,0 +1,10 @@
+public class Multiply {
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+
+        System.out.println("this is multiply" + (a*b));
+    }
+    
+}
